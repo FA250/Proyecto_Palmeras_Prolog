d57ca4ee979c20d0e9243f75b455c239b04416b7
ArlemGabriel <38301775+ArlemGabriel@users.noreply.github.com>
2019-05-18 22:15:11 -0600

    Add files via upload

--- Proyecto_Palmeras.pl
+++ Proyecto_Palmeras.pl
@@ -22,48 +22,36 @@ palm_needs(elisa,4).
 bucket(5).
 
 % tiempo minimo deseado por el jardinero
-desired_time(12).
+desired_time(114).
 
 % Cuando la lista está vacía todas las movidas fueron hechas pero estoy en una palmera
 % y el balde tiene agua hago solve_dfs para actualizar el balde
-solve_dfs(pwb(Posicion,Bucket,[],Movidas,Tiempo),[],Historia,TiempoPreFinal):-
-	Bucket \= 0,
-	Posicion \= well,
-	move(pwb(Posicion,Bucket,[],Historia,Tiempo),EstadoPreFinal,Movida),
-	update(EstadoPreFinal,_,EstadoFinal,TiempoFinal),
-	solve_dfs(EstadoFinal,[],Historia,TiempoFinal).
+solve_dfs(Estado,[],_,_) :- 
+	final_state(Estado).
 
-
-solve_dfs(Estado,[],Historia,Tiempo) :- 
-	final_state(Estado,Historia,Tiempo).
-
-solve_dfs(Estado,[Movida|Movidas],Historia,Tiempo) :-
+solve_dfs(Estado,[Movida|Movidas],[Movida|Movidas],Tiempo) :-
       move(Estado,Estado3,Movida),              % generar una nueva Movida
       update(Estado3,Movida,Estado2,Tiempo2),    % calcula nuevo estado usando Movida
       legal(Estado3,Estado2,Movida),                    % nuevo estado debe ser legal
-      solve_dfs(Estado2,Movidas,[Movida|Historia],Tiempo2).  % continuar a partir de nuevo estado
+      solve_dfs(Estado2,Movidas,Historia,Tiempo2).  % continuar a partir de nuevo estado
 
-test_dfs(Problema,Movidas) :-
+test_dfs(Problema,Historia) :-
       initial_state(Problema,Estado),
-	  solve_dfs(Estado,[jasmine,sheherazade,nina,elisa],Movidas,Tiempo).
+	  solve_dfs(Estado,[jasmine,sheherazade,nina,elisa],Historia,Tiempo).
 
 %Estado inicial del problema
 initial_state(pwb,pwb(well,Bucket,[jasmine,sheherazade,nina,elisa],[],0)) :-
 	Bucket is 0.
 	
 %Estado final del problema
-final_state(pwb(well,Bucket,[],Historia,Tiempo),Historia,Tiempo) :-
-	Bucket == 0,
+final_state(pwb(Posicion,Bucket,[],Historia,Tiempo)) :-
 	desired_time(TiempoDeseado),
-	Tiempo >=TiempoDeseado.
-
-%Actualiza la duración, la lista de palmeras y el balde una vez regada la palmera.
-update(pwb(Posicion,Bucket,Palmeras,Historia,Tiempo),Movida, pwb(Posicion,Bucket,Palmeras,Historia,Tiempo2),Tiempo2):-
-	Palmeras == [],
-	Tiempo2 is Tiempo.
+	Tiempo >=TiempoDeseado,
+	write('Tiempo: '),write(Tiempo),write('>'),write('Tiempo Deseado:'),write(TiempoDeseado).
 
 %Actualiza la duración, la lista de palmeras y el balde una vez regada la palmera.
 update(pwb(Posicion,Bucket,Palmeras,Historia,Tiempo),Movida, pwb(Movida,Bucket2,Palmeras2,Historia2,Tiempo2),Tiempo2):-
+	Palmeras \= [],
 	update_palmeras(Movida,Palmeras,Palmeras2,Historia,Historia2),
 	update_tiempo(Movida,Tiempo,Tiempo2),
 	update_bucket(Bucket,Bucket2,Movida). 
@@ -80,6 +68,7 @@ update_tiempo(Movida,Tiempo,Tiempo2):-
 update_bucket(Bucket,Bucket2,Movida):-
 	palm_needs(Movida,Cantidad),
 	Bucket2 is Bucket-Cantidad.
+
 %Movimiento final si el bucket tiene agua pero ya se regaron las palmeras
 %Se procede a ir al pozo y depositar el agua restante, actualizar tiempo.
 move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(well,BucketFinal,Movidas,Historia,TiempoFinal),Movida):-
@@ -110,10 +99,19 @@ move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,His
 
 %Moverse de una palmera al pozo, llenar el balde y volver a la palmera
 move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,Historia,Tiempo2),Movida):-
-	%Bucket == 0,
+	Bucket == 0,
 	bucket(Bucket2),
-	palm2well(Movida,Cantidad),
-	Tiempo2 is Tiempo+Bucket2+Cantidad+Cantidad.
+	palm2well(Posicion,Cantidad),
+	palm2palm(Posicion,Movida,Cantidad2),
+	Tiempo2 is Tiempo+Bucket2+Cantidad+Cantidad+Cantidad2.
+
+%Moverse de una palmera al pozo, llenar el balde y volver a la palmera
+move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,Historia,Tiempo2),Movida):-
+	Bucket == 0,
+	bucket(Bucket2),
+	palm2well(Posicion,Cantidad),
+	palm2palm(Movida,Posicion,Cantidad2),
+	Tiempo2 is Tiempo+Bucket2+Cantidad+Cantidad+Cantidad2.
 
 legal(Estado1,Estado,Movida):-
 	not(ilegal(Estado1,Estado,Movida)).
