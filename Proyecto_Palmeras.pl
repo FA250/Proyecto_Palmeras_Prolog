7497e202ffd9faff131a43cb23e482783706c7da
FA250 <faph.2009@hotmail.com>
2019-05-20 04:06:25 -0600

    Add files via upload

--- Proyecto_Palmeras.pl
+++ Proyecto_Palmeras.pl
@@ -18,120 +18,133 @@ palm_needs(sheherazade,1).
 palm_needs(nina,2).
 palm_needs(elisa,4).
 
-% tama√±o del balde
+% tamaÒo del balde
 bucket(5).
 
 % tiempo minimo deseado por el jardinero
 desired_time(114).
 
-% Cuando la lista est√° vac√≠a todas las movidas fueron hechas pero estoy en una palmera
+% Cuando la lista est· vacÌa todas las movidas fueron hechas pero estoy en una palmera
 % y el balde tiene agua hago solve_dfs para actualizar el balde
-solve_dfs(Estado,[],_,_) :- 
-	final_state(Estado).
+solve_dfs(pwb(well,0,[],Resultado,Tiempo),_,_,_) :- 
+	final_state(pwb(well,0,[],Resultado,Tiempo)).
 
-solve_dfs(Estado,[Movida|Movidas],[Movida|Movidas],Tiempo) :-
-      move(Estado,Estado3,Movida),              % generar una nueva Movida
-      update(Estado3,Movida,Estado2,Tiempo2),    % calcula nuevo estado usando Movida
-      legal(Estado3,Estado2,Movida),                    % nuevo estado debe ser legal
-      solve_dfs(Estado2,Movidas,Historia,Tiempo2).  % continuar a partir de nuevo estado
+solve_dfs(Estado,Movidas,Historia,Tiempo) :-
+      move(Estado,Movida2),              % generar una nueva Movida
+      update(Estado,Movida2,Estado2,Tiempo2,Movidas2),    % calcula nuevo estado usando Movida
+      legal(Estado2),                    % nuevo estado debe ser legal
+      solve_dfs(Estado2,Movidas2,Historia,Tiempo2).  % continuar a partir de nuevo estado
 
 test_dfs(Problema,Historia) :-
       initial_state(Problema,Estado),
 	  solve_dfs(Estado,[jasmine,sheherazade,nina,elisa],Historia,Tiempo).
 
 %Estado inicial del problema
-initial_state(pwb,pwb(well,Bucket,[jasmine,sheherazade,nina,elisa],[],0)) :-
-	Bucket is 0.
+initial_state(pwb,pwb(well,Bucket,[jasmine,sheherazade,nina,elisa],[],Bucket)) :-
+	 bucket(Bucket).
 	
 %Estado final del problema
-final_state(pwb(_,_,[],_,Tiempo)) :-
+final_state(pwb(_,_,[],Resultado,Tiempo)) :-
 	desired_time(TiempoDeseado),
 	Tiempo >=TiempoDeseado,
-	write('Tiempo: '),write(Tiempo),write('>'),write('Tiempo Deseado:'),write(TiempoDeseado).
+	write('Tiempo: '),write(Tiempo),write('>'),write('Tiempo Deseado:'),write(TiempoDeseado),
+	write('\nResultado: '),write(Resultado).
 
-%Actualiza la duraci√≥n, la lista de palmeras y el balde una vez regada la palmera.
-update(pwb(_,Bucket,Palmeras,Historia,Tiempo),Movida, pwb(Movida,Bucket2,Palmeras2,Historia2,Tiempo2),Tiempo2):-
-	Palmeras \= [],
-	update_palmeras(Movida,Palmeras,Palmeras2,Historia,Historia2),
-	update_tiempo(Movida,Tiempo,Tiempo2),
-	update_bucket(Bucket,Bucket2,Movida). 
+%Actualiza la duraciÛn, la lista de palmeras y el balde una vez regada la palmera.
+update(pwb(Posicion,Bucket,Palmeras,Historia,Tiempo),Movida, pwb(Movida,Bucket2,Palmeras2,Historia2,Tiempo2),Tiempo2,Palmeras2):-	
+	update_palmeras(Movida,Palmeras,Palmeras2,Historia,Historia2),	
+	update_bucket(Bucket,Bucket2,Movida,Palmeras),
+	update_tiempo(Posicion,Movida,Tiempo,Tiempo2,Palmeras,Bucket). 
 	
 
+update_palmeras(well,Palmeras,Palmeras,Historia,Historia).
+	
 update_palmeras(Movida,Palmeras,Palmeras2,Historia,Historia2):-
 	select(Movida,Palmeras,Palmeras2),
 	insert(Movida,Historia,Historia2).
 
-update_tiempo(Movida,Tiempo,Tiempo2):-
+update_tiempo(well,Movida,Tiempo,Tiempo2,_,_):-
 	palm_needs(Movida,Cantidad),
-	Tiempo2 is Tiempo+Cantidad.
+	palm2well(Movida,Cantidad2),
+	Tiempo2 is Tiempo+Cantidad+Cantidad2.
+	
+update_tiempo(Posicion,well,Tiempo,Tiempo2, Palmeras, Bucket):-	
+	Palmeras==[],
+	palm2well(Posicion,Cantidad2),
+	Tiempo2 is Tiempo+Bucket+Cantidad2.
+	
+update_tiempo(Posicion,well,Tiempo,Tiempo2,_,_):-	
+	bucket(Bucket),
+	palm2well(Posicion,Cantidad2),
+	Tiempo2 is Tiempo+Bucket+Cantidad2.
+	
+update_tiempo(Posicion,Movida,Tiempo,Tiempo2,_,_):-
+	palm_needs(Movida,Cantidad),
+	palm2palm(Posicion,Movida,Cantidad2),
+	Tiempo2 is Tiempo+Cantidad+Cantidad2.
+	
+update_tiempo(Posicion,Movida,Tiempo,Tiempo2,_,_):-
+	palm_needs(Movida,Cantidad),
+	palm2palm(Movida,Posicion,Cantidad2),
+	Tiempo2 is Tiempo+Cantidad+Cantidad2.
 
-update_bucket(Bucket,Bucket2,Movida):-
+update_bucket(Bucket,Bucket2,well,Palmeras):-	
+	Palmeras==[],
+	Bucket2=0.
+	
+update_bucket(Bucket,Bucket2,well,_):-	
+	bucket(Bucket2).
+	
+update_bucket(Bucket,Bucket2,Movida,_):-
 	palm_needs(Movida,Cantidad),
 	Bucket2 is Bucket-Cantidad.
 
 %Movimiento final si el bucket tiene agua pero ya se regaron las palmeras
 %Se procede a ir al pozo y depositar el agua restante, actualizar tiempo.
-move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(well,BucketFinal,Movidas,Historia,TiempoFinal),_):-
+move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),Movida):-
 	Movidas == [],
+	Movida=well.
+	
+%Moverse de una palmera al pozo, llenar el balde y volver a la palmera
+move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),Movida):-
+	Bucket == 0,
+	bucket(Bucket2),
 	palm2well(Posicion,Cantidad),
-	TiempoFinal is Tiempo+Cantidad+Bucket,
-	BucketFinal is 0.
+	Movida=well.
 
 %Moverse de una palmera a otra
-move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket,Movidas,Historia,Tiempo2),Movida):-
-	%Bucket \= 0,
-	palm2palm(Posicion,Movida,Cantidad),
-	Tiempo2 is Tiempo+Cantidad.
+move(pwb(Posicion,Bucket,[Movida|Movidas],Historia,Tiempo),Movida):-
+	Bucket \= 0,
+	palm2palm(Posicion,Movida,Cantidad).
+	
+move(pwb(Posicion,Bucket,[Movida|Movidas],Historia,Tiempo),Movida):-
+	Bucket \= 0,
+	palm2palm(Movida,Posicion,Cantidad).
 
-%Moverse de una palmera a otra
-move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket,Movidas,Historia,Tiempo2),Movida):-
-	%Bucket \= 0,
-	palm2palm(Movida,Posicion,Cantidad),
-	Tiempo2 is Tiempo+Cantidad.
-
-%Llenar el balde cuando est√° en 0
-move(pwb(Posicion,_,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,Historia,Tiempo2),Movida):-
+%Llenar el balde cuando est· en 0
+move(pwb(Posicion,_,[Movida|Movidas],Historia,Tiempo),Movida):-
 	Posicion == well,
 	%Bucket == 0,
-	bucket(Bucket2),
-	palm2well(Movida,Cantidad),
-	Tiempo2 is Tiempo+Bucket2+Cantidad.
+	palm2well(Movida,Cantidad).
 
-%Moverse de una palmera al pozo, llenar el balde y volver a la palmera
-move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,Historia,Tiempo2),Movida):-
-	Bucket == 0,
-	bucket(Bucket2),
-	palm2well(Posicion,Cantidad),
-	palm2palm(Posicion,Movida,Cantidad2),
-	Tiempo2 is Tiempo+Bucket2+Cantidad+Cantidad+Cantidad2.
 
-%Moverse de una palmera al pozo, llenar el balde y volver a la palmera
-move(pwb(Posicion,Bucket,Movidas,Historia,Tiempo),pwb(Movida,Bucket2,Movidas,Historia,Tiempo2),Movida):-
-	Bucket == 0,
-	bucket(Bucket2),
-	palm2well(Posicion,Cantidad),
-	palm2palm(Movida,Posicion,Cantidad2),
-	Tiempo2 is Tiempo+Bucket2+Cantidad+Cantidad+Cantidad2.
+	
 
-legal(Estado1,Estado,Movida):-
-	not(ilegal(Estado1,Estado,Movida)).
+legal(Estado):-
+	not(ilegal(Estado)).
 
 %Movimiento ilegal si el bucket no tiene suficiente agua para regar una palmera
-ilegal(pwb(_,Bucket1,_,_,_),pwb(Posicion,_,_,_,_),_):-
-	palm_needs(Posicion,Cantidad),
-	Resultado = Bucket1-Cantidad,
-	Resultado < 0.
+ilegal(pwb(Posicion,Bucket1,_,_,_)):-		
+	Bucket1 < 0.
 
-ilegal(pwb(Posicion1,Bucket1,_,_,_),pwb(Posicion,_,_,_,_),_):-
-	Bucket1 > 0,
-	Posicion1 == well,
+ilegal(pwb(Posicion,Bucket1,_,_,_)):-
+	bucket(Bucket1),
 	Posicion \= well.
 
 	
 select(X,[X|Xs],Xs).                          % Extrae primer elemento.
-select(X,[Y|Ys],[Y|Zs]):-select(X,Ys,Zs).    % Extrae elemento de m√°s adentro.
+select(X,[Y|Ys],[Y|Zs]):-select(X,Ys,Zs).    % Extrae elemento de m·s adentro.
 
 insert(X,[Y|Ys],[Y|Zs]):-
     insert(X,Ys,Zs).
 insert(X,[],[X]).
-	
